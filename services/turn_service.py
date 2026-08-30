3372367eceaaa95d31ed9076d8608e760df3a6da
tusanbot <mshmahyar9@gmail.com>
2026-08-30 11:12:50 +0330

    fix: align turn service with repository API

--- services/turn_service.py
+++ services/turn_service.py
@@ -2,11 +2,11 @@ class TurnService:
     def __init__(self, turn_repository):
         self.repo = turn_repository
 
-    async def start(self, **data):
-        return await self.repo.create_turn(**data)
+    def start(self, **data):
+        return self.repo.create_turn(**data)
 
-    async def finish(self, turn_id, **data):
-        return await self.repo.finish_turn(turn_id, **data)
+    def finish(self, turn_id, state=None):
+        return self.repo.finish_turn(turn_id, state=state)
 
-    async def history(self, game_id):
-        return await self.repo.list_turns(game_id)
+    def history(self, game_id):
+        return self.repo.list_turns(game_id)
